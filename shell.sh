d04b0917da341a39d0d9aa46b1370232dd16f81a
Swatipandey2006 <pandeyvarsha857@gmail.com>
2025-06-23 17:08:18 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,14 +1,2 @@
 echo "hello ${params.VARIABLE_NAME}"
 
-pipeline {
-    agent any
-
-    stages {
-        stage('Checkout Code') {
-            steps {
-                git branch: 'main',
-                    url: 'https://github.com/your-org/your-repo.git'
-            }
-        }
-    }
-}
